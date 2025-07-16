993acfd0291369c87217fbf896b5682a1b83a8df
Marie jose Ficani <ficanimariejose@gmail.com>
2025-07-16 10:59:18 +0300

    use bootstrap tooltip for slot buttons in room component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
+
+// bootstrapApplication(App, appConfig)
+//   .catch((err) => console.error(err));
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import * as bootstrap from 'bootstrap';
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tooltipTriggerList = Array.from(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach((el) => new bootstrap.Tooltip(el));
+});
